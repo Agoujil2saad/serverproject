6c15edbeb66b52788b4b0697af9564575f108d6a
Agoujil2saad <agoujilsaad@gmail.com>
2017-11-05 13:07:48 +0000

    initialzing the project

---
+++ src/com/project/configuration/DBConfiguration.java
@@ -0,0 +1,8 @@
+package com.project.configuration;
+
+public class DBConfiguration {
+    public static String uid = "root";
+    public static String pwd = "";
+    public static String cat = "ProductStore";
+    public static String mysql_db_url = "jdbc:mysql://localhost/ProductStore";
+}
