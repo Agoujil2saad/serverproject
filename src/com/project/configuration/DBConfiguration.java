20e8c43adf4e2689c5a0d92dbb18b01bf48123eb
Marwane Chahoud <marwane.chahoud@gmail.com>
2017-11-07 18:21:31 +0000

    add files to .ignorefile file

--- src/com/project/configuration/DBConfiguration.java
+++ src/com/project/configuration/DBConfiguration.java
@@ -4,5 +4,5 @@ public class DBConfiguration {
     public static String uid = "root";
     public static String pwd = "";
     public static String cat = "ProductStore";
-    public static String mysql_db_url = "jdbc:mysql://localhost/ProductStore";
+    public static String mysql_db_url = "jdbc:mysql://localhost/ProductStore?useLegacyDatetimeCode=false&serverTimezone=CEST";
 }
